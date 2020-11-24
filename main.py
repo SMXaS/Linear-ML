0764e258f0504ac8bf8bf151f00b1672580bd9ba
SMXaS <jurkisarnis@gmail.com>
2020-11-24 20:18:12 +0000

    Documentation 2x

--- main.py
+++ main.py
@@ -8,19 +8,23 @@ from sklearn import linear_model
 data = pd.read_csv("student-mat.csv", sep=";")
 data = data[["G1", "G2", "G3", "studytime", "failures", "absences"]]
 predict = "G3"
+
 """Storing Data with Numpy"""
 X = np.array(data.drop([predict], 1))
 Y = np.array(data[predict])
+
 """Preparing Testing and Training"""
 x_train, x_test, y_train, y_test = sklearn.model_selection.train_test_split(X, Y, test_size=0.1)
 linear = linear_model.LinearRegression()
 linear.fit(x_train, y_train)
+
 """Getting Accuracy"""
 accuracy = linear.score(x_test, y_test)
 print(accuracy)
+
+"""A view to see where the Algorithm fails"""
 print("Coefficient: \n", linear.coef_)
 print("Intercept \n", linear.intercept_)
-"""A view to see where the Algorithm fails"""
 predictions = linear.predict(x_test)
 for x in range(len(predictions)):
     print(predictions[x], x_test[x], y_test[x])
